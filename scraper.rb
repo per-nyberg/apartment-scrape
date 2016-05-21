9bc7873894684f2f2b88dcd0aff12f6ba075286c
Per <per@per-nyberg.se>
2016-05-21 17:49:47 +0200

    pry

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'pry'
 require 'csv'
 
-page = 'https://www.blocket.se/stockholm?q=l%C3%A4genhet&cg=0&w=1&st=s&c=&ca=11&is=1&l=0&md=th'
+# page to scrape
+# alternative page: https://www.blocket.se/stockholm?q=l%C3%A4genhet&cg=0&w=1&st=s&c=&ca=11&is=1&l=0&md=th
+page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
 
-unwanted_commited_change = "asd"
+Pry.start(binding)
